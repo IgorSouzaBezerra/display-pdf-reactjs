4e3b139467f7b4e6c21efbb24daf618fd2fe8fbe
Igor <igor_csns_s2@hotmail.com>
2021-11-27 12:56:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import CloudPdfViewer from '@openbook/cloudpdf-viewer';
+
+import './styles.css';
 
 function App() {
+
+  const viewer = useRef(null);
+  useEffect(() => {
+    CloudPdfViewer(
+      {
+        documentId: "c2e4e29b-b7ba-4d87-99dd-126b671fd19d",
+        darkMode: true
+      },
+      viewer.current
+    ).then((instance) => {});
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="viewer" ref={viewer}></div>
     </div>
   );
 }
